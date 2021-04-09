e00fd0de148ed79fafd9fd48a229825aefc23ab6
Bytangle <dev.bytangle@gmail.com>
2021-04-09 22:43:07 +0100

    Implement TextRecognition, just logging the output

--- app/src/main/java/dev/bytangle/airtime/dbl/AirtimeImageAnalyzer.kt
+++ app/src/main/java/dev/bytangle/airtime/dbl/AirtimeImageAnalyzer.kt
@@ -1,16 +1,30 @@
 package dev.bytangle.airtime.dbl
 
+import android.util.Log
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
 
 class AirtimeImageAnalyzer : ImageAnalysis.Analyzer {
     @ExperimentalGetImage
     override fun analyze(imageProxy: ImageProxy) {
         val mediaImage = imageProxy.image
         if (mediaImage != null) {
-            //val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-            // ML Text Recognizer code
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            val recognizer = TextRecognition.getClient()
+
+            // recognition block
+            recognizer.process(image).addOnSuccessListener { visionText ->
+                val resultText = visionText.text;
+                Log.d("TextRecognition", resultText)
+            }.addOnFailureListener { err ->
+
+            }.addOnCompleteListener {
+                imageProxy.close()
+            }
+
         } else {
 
         }
