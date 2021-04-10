9ddf5c307a70b21a66aa4cdb6ea9501756443db5
Bytangle <dev.bytangle@gmail.com>
2021-04-10 12:00:10 +0100

    Implement substring extraction helper functions

--- app/src/main/java/dev/bytangle/airtime/utils/Utils.kt
+++ app/src/main/java/dev/bytangle/airtime/utils/Utils.kt
@@ -45,6 +45,34 @@ enum class AirtimeRechargeStatus {
     INVALID, USED, LOADED, NETWORK_ISSUE
 }
 
+/*
+Extraction Functions
+ */
+
+//fun determineRechargePinNetwork(rawInput : CharSequence) : AirtimeSupportedNetwork {
+//    when {
+//
+//    }
+//}
+
+fun extractRechargePin(rawInput : String) : CharSequence? {
+    return filter(rawInput, AirtimeFilterPatterns.RECHARGE_PIN_REGEX).firstOrNull()?.let {
+        return extractSubstringUsingFirstAndLastIndices(it.value, getFirstAndLastIndexOfInts(it.value))
+    }
+}
+
+fun extractRechargePinPrefix(rawInput: String) : CharSequence? {
+    return filter(rawInput, AirtimeFilterPatterns.RECHARGE_PIN_PREFIX_REGEX).firstOrNull()?.let {
+        return extractSubstringUsingFirstAndLastIndices(it.value, getFirstAndLastIndexOfChar(it.value, "\\*"))
+    }
+}
+
+fun extractRechargeAmount(rawInput : String) : CharSequence? {
+    return filter(rawInput, AirtimeFilterPatterns.AMOUNT_REGEX).firstOrNull()?.let {
+        return extractSubstringUsingFirstAndLastIndices(it.value, getFirstAndLastIndexOfInts(it.value))
+    }
+}
+
 object AirtimeFilterPatterns {
     val RECHARGE_PIN_PREFIX_REGEX = Regex("^(\\s+|\\w+)?(\\s+|\\w+)?(\\s+|\\w+)?\\*\\d+\\*(\\s+)?(\\d+|\\w+)+(\\s+)?#?(\\s+)?$", setOf(RegexOption.MULTILINE))
     val RECHARGE_PIN_REGEX = Regex("^(\\s+|\\w+)?(\\s+|\\w+)?(\\s+|\\w+)?\\d{3,5}(((-|\\s)\\d{3,5})+)?((-|\\s)\\d{3,5})(\\s+)?$", RegexOption.MULTILINE)
@@ -58,8 +86,8 @@ object AirtimeFilterPatterns {
     val AIRTEL_RECHARGE_PIN_REGEX = Regex("^\\d{4}(-|\\s)\\d{4}(-|\\s)\\d{4}(-|\\s)\\d{4}\$", RegexOption.MULTILINE)
 }
 
-fun filter(chunks : String, spec : Regex) : Sequence<MatchResult> {
-    return spec.findAll(chunks)
+fun filter(rawInput: String, spec : Regex) : Sequence<MatchResult> {
+    return spec.findAll(rawInput)
 }
 
 fun extractSubstringUsingFirstAndLastIndices(input : CharSequence, pair: Pair<Int, Int>) : CharSequence {
@@ -68,10 +96,19 @@ fun extractSubstringUsingFirstAndLastIndices(input : CharSequence, pair: Pair<In
 
 fun getFirstAndLastIndexOfInts(input : CharSequence) : Pair<Int, Int> {
     val isIntRegex = Regex("\\d")
+    return getFirstAndLastIndexOfRegexSpec(input, isIntRegex)
+}
+
+fun getFirstAndLastIndexOfChar(input : CharSequence, pattern : CharSequence) : Pair<Int, Int> {
+    val isCharRegex = Regex("$pattern")
+    return getFirstAndLastIndexOfRegexSpec(input, isCharRegex)
+}
+
+private fun getFirstAndLastIndexOfRegexSpec(input : CharSequence, regex: Regex) : Pair<Int, Int> {
     var firstIntIndex : Int = -1
     var lastIntIndex : Int = -1
     input.forEachIndexed { index, char ->
-        if (isIntRegex.matches(char.toString())) {
+        if (regex.matches(char.toString())) {
             if (firstIntIndex < 0) firstIntIndex = index
             lastIntIndex = index
         }
