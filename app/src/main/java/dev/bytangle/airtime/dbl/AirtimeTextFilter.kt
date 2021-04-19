4179ecedf6c1aee9c59fc0e9a063f715c484d920
Bytangle <dev.bytangle@gmail.com>
2021-04-19 23:07:14 +0100

    Use Textblocks to retrieve recharge pin and other info

--- app/src/main/java/dev/bytangle/airtime/dbl/AirtimeTextFilter.kt
+++ app/src/main/java/dev/bytangle/airtime/dbl/AirtimeTextFilter.kt
@@ -1,12 +1,46 @@
 package dev.bytangle.airtime.dbl
 
+import android.util.Log
 import com.google.mlkit.vision.text.Text
 import dev.bytangle.airtime.utils.*
 
 object AirtimeTextFilter {
-    private lateinit var chunks : String
     fun process(text : Text) : AirtimeProcessedResult {
-        chunks = text.text
+        return processUsingTextBlocks(text.textBlocks)
+        //return processUsingFulltext(text.text)
+    }
+
+    private fun processUsingTextBlocks(blocks : List<Text.TextBlock>): AirtimeProcessedResult {
+
+        var rechargePin : CharSequence? = null
+        var rechargePinPrefix : CharSequence? = null
+        var rechargeAmount : CharSequence? = null
+
+        blocks.forEach { textBlock ->
+            textBlock.lines.forEach { textLine ->
+                if (rechargePin == null) {
+                    rechargePin = extractRechargePin(textLine.text)
+                }
+                if (rechargePinPrefix == null) {
+                    rechargePinPrefix = extractRechargePinPrefix(textLine.text)
+                }
+                if (rechargeAmount == null) {
+                    rechargeAmount = extractRechargeAmount(textLine.text)
+                }
+            }
+        }
+
+        val assumedRechargePinNetworkAndPrefix = determineRechargePinNetworkAndPrefix(rechargePin = rechargePin ?: "")
+
+        return AirtimeProcessedResult(
+            assumedNetwork = assumedRechargePinNetworkAndPrefix.network,
+            rechargePin = rechargePin ?: "",
+            amount = rechargeAmount ?: "",
+            pinPrefix = assumedRechargePinNetworkAndPrefix.prefix
+        )
+    }
+
+    private fun processUsingFulltext(chunks : String) : AirtimeProcessedResult {
         val rechargePin = extractRechargePin(chunks)
         val rechargePinPrefix = extractRechargePinPrefix(chunks)
         val rechargeAmount = extractRechargeAmount(chunks)
--- app/src/main/java/dev/bytangle/airtime/viewmodels/ScanViewModel.kt
+++ app/src/main/java/dev/bytangle/airtime/viewmodels/ScanViewModel.kt
@@ -2,15 +2,17 @@ package dev.bytangle.airtime.viewmodels
 
 import android.content.Context
 import android.graphics.PointF
+import android.graphics.Rect
+import android.location.Location
 import android.os.Build
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.core.app.ComponentActivity
+import androidx.core.view.drawToBitmap
 import androidx.lifecycle.ViewModel
 import com.otaliastudios.cameraview.CameraView
-import com.otaliastudios.cameraview.controls.Audio
-import com.otaliastudios.cameraview.controls.Flash
+import com.otaliastudios.cameraview.controls.*
 import com.otaliastudios.cameraview.gesture.Gesture
 import com.otaliastudios.cameraview.gesture.GestureAction
 import com.otaliastudios.cameraview.markers.AutoFocusMarker
@@ -27,13 +29,16 @@ class ScanViewModel : ViewModel() {
         camera.setLifecycleOwner(activity)
         camera.audio = Audio.OFF
         camera.flash = flashLightState
+        camera.engine = Engine.CAMERA2
+        camera.frameProcessingPoolSize = 2
+        camera.exposureCorrection = 2F
+        camera.mode = Mode.PICTURE
 
-        camera.setPreviewStreamSize(SizeSelector {
+        camera.setPreviewStreamSize {
             listOf(it[4])
-        })
+        }
 
         camera.isFocusedByDefault = true
-
         // Gesture setup
         camera.mapGesture(Gesture.TAP, GestureAction.AUTO_FOCUS)
 
